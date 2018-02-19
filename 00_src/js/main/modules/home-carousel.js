e9de1aa906f4fb1536054c7244ddebfc6068a507
Laurianne <laurianne@spintank.fr>
2018-02-19 16:39:42 +0100

    Expand image when clicked

--- 00_src/js/main/modules/home-carousel.js
+++ 00_src/js/main/modules/home-carousel.js
@@ -11,6 +11,8 @@ const homeCarousel = {
 
             this.ui.$galleryContainer = document.querySelectorAll('.js-gallery');
             this.ui.$galleryItems =  document.querySelectorAll('.js-gallery-item');
+            this.ui.$carousel = document.querySelector('.js-carousel');
+            this.ui.$clone = document.querySelector('.js-carousel-clone-container');
         },
 
         setProperties() {
@@ -35,13 +37,15 @@ const homeCarousel = {
             for (let item of this.ui.$galleryItems) {
                 item.addEventListener('click', this.onClick.bind(this));
             }
-            window.addEventListener('parallax:disabled', this.zoomItem.bind(this));
+            // window.addEventListener('parallax:disabled', this.zoomItem.bind(this));
         },
 
         onClick(e) {
+            console.log('what?');
             if (!this.isZoomed) {
-                // this.zoomItem(e.target);
-                this.itemActive = e.target;
+                console.log('what?');
+                this.zoomItem(e.target);
+                // this.itemActive = e.target;
                 this.disableParallax();
             }
         },
@@ -52,8 +56,41 @@ const homeCarousel = {
             window.dispatchEvent(evt);
         },
 
-        zoomItem() {
-            console.log('zoom');
+        zoomItem(item) {
+            var imgSrcUrl = item.getAttribute('src');
+            var imgSrc = document.createElement('img');
+            var imgClone = this.ui.$clone;
+
+            imgSrc.src = imgSrcUrl;
+            imgClone.appendChild(imgSrc);
+
+            this.position.itemX = item.getBoundingClientRect().left;
+            this.position.itemY = item.getBoundingClientRect().top;
+            this.position.itemWidth = item.offsetWidth;
+            this.position.itemHeight = item.offsetHeight;
+            this.position.itemCenterY = this.position.itemY - this.position.itemHeight / 2;
+            this.position.itemCenterX = this.position.itemX - this.position.itemWidth / 2;
+
+            imgClone.style.top = this.position.itemY + 'px';
+            imgClone.style.left = this.position.itemX + 'px';
+            imgClone.style.height = this.position.itemHeight + 'px';
+            imgClone.style.width = this.position.itemWidth + 'px';
+
+            imgClone.classList.add('is-zoomed');
+
+            setTimeout(() => {
+                this.ratio = this.position.windowHeight / this.position.windowWidth < this.position.itemHeight / this.position.itemWidth ?
+                    this.position.windowHeight * 0.9 / this.position.itemHeight
+                    : this.position.windowWidth * 0.9 / this.position.itemWidth;
+                imgClone.style.height = this.position.itemHeight * this.ratio + 'px';
+                imgClone.style.width = this.position.itemWidth * this.ratio + 'px';
+                imgClone.style.top = this.position.windowY - this.position.itemHeight * this.ratio / 2 + 'px';
+                imgClone.style.left = this.position.windowX - this.position.itemWidth *this.ratio / 2  + 'px';
+                // console.log(ratio);
+                // if (this.position.itemHeight > this.position.itemWidth) {
+                //
+                // }
+            }, 250)
 
             this.isZoomed = true;
         }
